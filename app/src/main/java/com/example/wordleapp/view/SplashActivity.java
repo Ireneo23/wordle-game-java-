6a2df04bcba12d6ebb20255cbd8effa73945bc61
Ireneo23 <madohinog123@gmail.com>
2025-04-27 13:40:17 +0800

    commitsso

---
+++ app/src/main/java/com/example/wordleapp/view/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.wordleapp.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.wordleapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity after a delay
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // 2 seconds delay
+    }
+}
